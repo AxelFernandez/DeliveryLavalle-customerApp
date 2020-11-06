b3ac395bf72730a2c4e627b3ae0c9be9781e0125
AxelFernandez <axel.fernandez0145@gmail.com>
2020-11-06 19:03:21 -0300

    Adding handling to notifications

--- app/src/main/java/com/axelfernandez/deliverylavalle/utils/MyFirebaseMessageService.kt
+++ app/src/main/java/com/axelfernandez/deliverylavalle/utils/MyFirebaseMessageService.kt
@@ -1,7 +1,6 @@
 package com.axelfernandez.deliverylavalle.utils
 
 
-import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -11,15 +10,14 @@ import android.content.Intent
 import android.media.RingtoneManager
 import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
-import com.axelfernandez.deliverylavalle.MainActivity
+import com.axelfernandez.deliverylavalle.HomeActivity
 import com.axelfernandez.deliverylavalle.R
 import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.util.*
 
 
 class MyFirebaseMessageService : FirebaseMessagingService() {
@@ -27,33 +25,43 @@ class MyFirebaseMessageService : FirebaseMessagingService() {
 
     val TAG = "FirebaseMessagingService"
 
-    @RequiresApi(Build.VERSION_CODES.M)
-    @SuppressLint("LongLogTag")
+
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
 
         if (remoteMessage.notification != null) {
             showNotification(remoteMessage.notification?.title, remoteMessage.notification?.body)
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
     private fun showNotification(title: String?, body: String?) {
-        val intent = Intent(this, MainActivity::class.java)
+        val intent = Intent(this, HomeActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(this, 0, intent,
             PendingIntent.FLAG_ONE_SHOT)
 
         val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationBuilder = NotificationCompat.Builder(this)
-            .setSmallIcon(R.mipmap.ic_launcher_foreground)
-            .setContentTitle(title)
-            .setContentText(body)
-            .setAutoCancel(true)
-            .setSound(soundUri)
-            .setContentIntent(pendingIntent)
-
+        val notificationBuilder = NotificationCompat.Builder(this,"other").apply {
+            setSmallIcon(R.mipmap.ic_launcher)
+            setContentTitle(title)
+            setContentText(body)
+            setAutoCancel(true)
+            setSound(soundUri)
+            setContentIntent(pendingIntent)
+        }
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(0, notificationBuilder.build())
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "other"
+            val channel = NotificationChannel(
+                channelId,
+                "otherChannel",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(channel)
+            notificationBuilder.setChannelId(channelId)
+        }
+        notificationManager.notify(Random().nextInt(), notificationBuilder.build())
     }
 
 
